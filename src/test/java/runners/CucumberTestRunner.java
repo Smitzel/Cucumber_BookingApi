90b66c81b4615e131ed933ffeebb4ff862cee896
Marcel S <marcelsmitsneek@gmail.com>
2025-04-10 15:46:26 +0200

    added extent report

--- src/test/java/runners/CucumberTestRunner.java
+++ src/test/java/runners/CucumberTestRunner.java
@@ -2,6 +2,10 @@ package runners;
 
 import io.cucumber.junit.platform.engine.Constants;
 import org.junit.platform.suite.api.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
@@ -9,9 +13,12 @@ import org.junit.platform.suite.api.*;
 @ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "steps")
 @ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME, value = "@booking")
 @ConfigurationParameter(key = Constants.EXECUTION_DRY_RUN_PROPERTY_NAME, value = "false")
+//@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME,value = "pretty, html:target/cucumber-report/cucumber.html")
 @ConfigurationParameter(
-        key = Constants.PLUGIN_PROPERTY_NAME,
-        value = "pretty, html:target/cucumber-report/cucumber.html"
+        key = PLUGIN_PROPERTY_NAME,
+        value = "pretty, com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:target/extent-report"
 )
 public class CucumberTestRunner {
+
+
 }
